b829f54816cca5d58f27bab1df93749c4bcc244f
Michal92299 <michal.99967@gmail.com>
2025-07-20 13:49:57 +0200

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,42 +1,19 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-console.log('Preload script loaded');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    send: (channel, data) => {
-        const validChannels = [
-            'launch-app',
-            'set-volume',
-            'set-brightness',
-            'toggle-wifi',
-            'toggle-bluetooth',
-            'connect-wifi',
-            'get-wifi-list',
-            'get-bluetooth-list',
-            'get-initial-settings',
-            'show-hacker-menu'
-        ];
-        if (validChannels.includes(channel)) {
-            console.log(`Sending IPC message on channel: ${channel}`, data);
-            ipcRenderer.send(channel, data);
-        } else {
-            console.error(`Invalid IPC send channel: ${channel}`);
-        }
-    },
-    on: (channel, callback) => {
-        const validChannels = ['wifi-list', 'bluetooth-list', 'initial-settings'];
-        if (validChannels.includes(channel)) {
-            console.log(`Registering IPC listener for channel: ${channel}`);
-            ipcRenderer.on(channel, (event, ...args) => {
-                try {
-                    console.log(`Received IPC message on channel: ${channel}`, args);
-                    callback(...args);
-                } catch (error) {
-                    console.error(`Error in IPC callback for channel ${channel}:`, error);
-                }
-            });
-        } else {
-            console.error(`Invalid IPC receive channel: ${channel}`);
-        }
-    }
+contextBridge.exposeInMainWorld('electronApi', {
+    launchApp: (appName) => ipcRenderer.invoke('launchApp', appName),
+                                systemAction: (action) => ipcRenderer.invoke('systemAction', action),
+                                launchSettings: () => ipcRenderer.invoke('launchSettings'),
+                                audioAction: (action) => ipcRenderer.invoke('audioAction', action),
+                                displayAction: (action) => ipcRenderer.invoke('displayAction', action),
+                                networkAction: (action) => ipcRenderer.invoke('networkAction', action),
+                                powerAction: (profile) => ipcRenderer.invoke('powerAction', profile),
+                                setLanguage: (newLang) => ipcRenderer.invoke('setLanguage', newLang),
+                                selectWifi: (ssid) => ipcRenderer.invoke('selectWifi', ssid),
+                                connectWifi: () => ipcRenderer.invoke('connectWifi'),
+                                scanBluetooth: () => ipcRenderer.invoke('scanBluetooth'),
+                                selectBluetooth: (deviceId) => ipcRenderer.invoke('selectBluetooth', deviceId),
+                                pairBluetooth: () => ipcRenderer.invoke('pairBluetooth'),
+                                closeSettings: () => ipcRenderer.invoke('closeSettings'),
+                                initSettings: () => ipcRenderer.invoke('initSettings')
 });
