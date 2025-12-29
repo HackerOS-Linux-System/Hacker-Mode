709e713a38b7d6161fc8c8ae907d134c1dc7d9b1
michal92299 <chudybyczek2137@gmail.com>
2025-12-29 18:12:03 +0100

    Update preload.js

--- source-code/preload.js
+++ source-code/preload.js
@@ -1,1 +1,7 @@
+const { contextBridge, ipcRenderer } = require('electron');
+contextBridge.exposeInMainWorld('electronApi', {
+    launchApp: (appName) => ipcRenderer.invoke('launchApp', appName),
+                                systemAction: (action) => ipcRenderer.invoke('systemAction', action),
+                                launchSettings: () => ipcRenderer.invoke('launchSettings')
+});
 
