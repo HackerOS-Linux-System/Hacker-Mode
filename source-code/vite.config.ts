22194880a872df29b3e3645ac9042255908adf33
michal92299 <chudybyczek2137@gmail.com>
2026-03-20 19:24:01 +0100

    Update vite.config.ts

--- source-code/vite.config.ts
+++ source-code/vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-                            base: './', // Important for Electron to find assets
+                            base: './',
                             server: {
-                                port: 3000,
-                            }
+                                port: 5173,
+                                strictPort: true,
+                            },
+                            build: {
+                                outDir: 'dist',
+                            emptyOutDir: true,
+                            },
 });
