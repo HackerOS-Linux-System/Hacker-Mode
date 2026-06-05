d0cad629e6104912c8c6691f948be1a231963416
michal92299 <chudybyczek2137@gmail.com>
2026-06-05 08:09:06 +0200

    Create mod.rs

---
+++ core/src/imports/mod.rs
@@ -0,0 +1,6 @@
+pub use crate::store::steam::SteamImporter;
+pub use crate::store::epic::EpicImporter;
+pub use crate::store::gog::GogImporter;
+pub use crate::store::amazon::AmazonImporter;
+pub use crate::store::itchio::ItchioImporter;
+pub use crate::store::lutris::LutrisImporter;
