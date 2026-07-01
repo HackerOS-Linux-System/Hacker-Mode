9bb76d3c2b13db4c52aba79a43541c82e4237b7a
michal92299 <chudybyczek2137@gmail.com>
2026-07-01 22:53:05 +0200

    Delete core directory

--- core/src/imports/mod.rs
+++
@@ -1,6 +0,0 @@
-pub use crate::store::steam::SteamImporter;
-pub use crate::store::epic::EpicImporter;
-pub use crate::store::gog::GogImporter;
-pub use crate::store::amazon::AmazonImporter;
-pub use crate::store::itchio::ItchioImporter;
-pub use crate::store::lutris::LutrisImporter;
--- core/src/settings/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod config;
-pub mod power;
-pub mod network;
-pub use config::Config;
